aa44a279304ac49418dc99efa860725ca3689e45
Yasser Bouaabane <b.yasser@Alexsys.solutions>
2023-03-24 11:23:57 +0000

    Add RabbitMQ

---
+++ src/BuildingBlocks/EventBus/EventBus/Abstractions/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace EventBus.Abstractions;
+public interface IDynamicIntegrationEventHandler
+{
+    Task Handle(dynamic eventData);
+}
---
+++ src/BuildingBlocks/EventBus/EventBus/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.Text.Json.Serialization;
+global using EventBus.Events;
+global using EventBus.Abstractions;
+global using static EventBus.InMemoryEventBusSubscriptionsManager;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+global using System;
